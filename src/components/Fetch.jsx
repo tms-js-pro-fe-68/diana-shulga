bfdce2486bc9eb5f4f77de0cfe8273aeb0e5be81
Dina Shulga <joker_1357@mail.ru>
2022-05-23 22:22:56 +0300

    Hook

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,31 +1,44 @@
-import {useState, useEffect} from "react";
+import { useEffect, useState } from "react"
 
 
-export default function Fetch(){
-    const [isLoading, setIsLoading] = useState(true)
-    const [facts, setNews] = useState([])
-    const [errorMessage, setErrorMessage] = useState("")
+function Hook(https){
+    const [fetchData, setFetchData] = useState([])
+    const [error, setError] = useState("")
+    const [change, setChange] = useState(false)
+    const [loading, setLoading] = useState(false)
     
     useEffect(() => {
-        setIsLoading(true)
-        fetch("https://www.boredapi.com/api/activity/")
+        setLoading(true)
+        fetch(https)
         .then((response) => {
             if(response.status !== 200){
                 throw Error(response.statusText)
             }
             return response.json()})
         .then((data) => {
-            setNews(data)
+            setFetchData(data)
         })
-        .catch((error) => setErrorMessage(error.message))
-        .finally(() => setIsLoading(false));
-    }, []);
+        .catch((errors) => setError(errors.message))
+        .finally(() => {
+            setLoading(false)
+        });
+    }, [change]);
+
+    return {fetchData, error, loading, setChange, change}
+}
+
+export default function Fetch(){
+
+    const {fetchData: news, error: errorMessage, loading, setChange, change} = Hook("https://www.boredapi.com/api/activity/");
+
+
 
     return (
-        <div className="facts">
-            {isLoading && <div>Loading...</div>}
-            <div>{errorMessage}</div>
-            <div className="choosing-fact">{facts.activity}</div>
+        <div className="container">
+            {!!errorMessage && !news.activity ? <div>{errorMessage}</div> : ""}
+                {loading && !news.activity ? <div className="group-name">Loading...</div> : ""}
+                <div className="name">{news.activity}</div>
+            <div role="button" tabIndex={0} className="button" onClick={() => change ? setChange(false) : setChange(true) }>just something</div>
         </div>
-    )
-}
+    );
+};
